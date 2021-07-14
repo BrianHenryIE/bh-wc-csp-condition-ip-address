f6243f777b03cde479d1a2f73cfadc245539829c
Brian Henry <BrianHenryIE@gmail.com>
2021-07-13 17:24:00 -0700

    Update class-wc-csp-conditions-Test.php

--- tests/unit/Woocommerce_Conditional_Shipping_And_Payments/class-wc-csp-conditions-Test.php
+++ tests/unit/Woocommerce_Conditional_Shipping_And_Payments/class-wc-csp-conditions-Test.php
@@ -4,19 +4,48 @@ namespace BrianHenryIE\WC_CSP_Condition_IP_Address\WooCommerce_Conditional_Shipp
 
 class WC_CSP_Conditions_Test extends \Codeception\Test\Unit {
 
-	/**
-	 * Verify the filter that will add the condition correctly adds the class name to the string[].
-	 */
-	public function test_add_condition() {
+    /**
+     * Verify the filter that will add the conditions correctly adds the class name to the string[].
+     */
+    public function test_add_ip_address_condition() {
 
-		$sut = new WC_CSP_Conditions();
+        $sut = new WC_CSP_Conditions();
 
-		$result = $sut->add_condition( array() );
+        $result = $sut->add_condition( array() );
 
-		$this->assertIsString( $result[0] );
+        $this->assertIsString( $result[0] );
 
-		$this->assertContains( 'BH_WC_CSP_Condition_IP_Address\woocommerce_conditional_shipping_and_payments\WC_CSP_Condition_IP_Address', $result );
+        error_log( json_encode( $result ));
 
-	}
+        $this->assertContains( 'BrianHenryIE\WC_CSP_Condition_IP_Address\WooCommerce_Conditional_Shipping_And_Payments\WC_CSP_Condition_IP_Address', $result );
+
+    }
+
+
+    /**
+     * Verify the filter that will add the conditions correctly adds the class name to the string[].
+     */
+    public function test_add_vpn_condition() {
+
+        $sut = new WC_CSP_Conditions();
+
+        $result = $sut->add_condition( array() );
+
+        $this->assertContains( 'BrianHenryIE\WC_CSP_Condition_IP_Address\WooCommerce_Conditional_Shipping_And_Payments\WC_CSP_Condition_Behind_VPN', $result );
+
+    }
+
+    /**
+     * Verify the filter that will add the conditions correctly adds the class name to the string[].
+     */
+    public function test_add_is_mobile_condition() {
+
+        $sut = new WC_CSP_Conditions();
+
+        $result = $sut->add_condition( array() );
+
+        $this->assertContains( 'BrianHenryIE\WC_CSP_Condition_IP_Address\WooCommerce_Conditional_Shipping_And_Payments\WC_CSP_Condition_Browser', $result );
+
+    }
 
 }
